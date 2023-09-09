d3373605783afb5a83d9ef54e96a376792ad0f30
Yuya Ogawa <yuya.ogw@gmail.com>
2023-09-09 11:38:47 +0900

    Fix invoice

--- src/wallet/invoice.rs
+++ src/wallet/invoice.rs
@@ -4,20 +4,21 @@ use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Deserialize, Serialize)]
-pub struct Bech32Invoice {
-    bech32_invoice: String,
+pub struct RgbInvoice {
+    invoice_string: String,
 }
 
 #[put("/wallet/invoice")]
-pub async fn put(params: web::Json<Bech32Invoice>) -> impl Responder {
-    let decoded = Invoice::new(params.bech32_invoice.clone());
+pub async fn put(params: web::Json<RgbInvoice>) -> impl Responder {
+    let decoded = Invoice::new(params.invoice_string.clone());
     match decoded {
         Ok(invoice) => HttpResponse::Ok().json(InvoiceData {
+            asset_iface: invoice.invoice_data().asset_iface,
             blinded_utxo: invoice.invoice_data().blinded_utxo,
             asset_id: invoice.invoice_data().asset_id,
             amount: invoice.invoice_data().amount,
             expiration_timestamp: invoice.invoice_data().expiration_timestamp,
-            consignment_endpoints: invoice.invoice_data().consignment_endpoints,
+            transport_endpoints: invoice.invoice_data().transport_endpoints,
         }),
         Err(e) => HttpResponse::BadRequest().body(e.to_string()),
     }
@@ -28,12 +29,13 @@ mod tests {
     use super::*;
 
     use actix_web::{http, test, App};
+    use rgb_lib::wallet::BlindedUTXO;
 
     #[actix_web::test]
-    async fn test_put() {
+    async fn test_put_invoice() {
         let app = test::init_service(App::new().service(put)).await;
-        let payload = Bech32Invoice {
-            bech32_invoice: ("i1q93kqcyuklwuz3z87zwudx28tutzduwg0jd8tmz6vpt2pcxm5h8h7llntcek2vsnqvesxprsxz7pzc6wqxcm3gpztgxgcryv4gxpjff9qhz4d7h6q4zlj9v402v5txw9ukynjwdfy4avn7delfvut7tehfzstjgqwhe0f0")
+        let payload = RgbInvoice {
+            invoice_string: ("rgb:9usESnQYgX2KWNycD3cYRGddBc65uDC6gPeHjV9XzbHU/RGB20/10+DLrwJdhSdhhhxrUGZudY6C6ubbdPn14SZ1FLuTT3nUER?expiry=1694222774&endpoints=rpc://127.0.0.1:3000/json-rpc")
                 .to_string(),
         };
         let req = test::TestRequest::put()
@@ -44,14 +46,15 @@ mod tests {
         println!("{:?}", resp);
         assert!(resp.status().is_success());
         let body: InvoiceData = test::read_body_json(resp).await;
-        assert!(body.blinded_utxo.starts_with("txo"));
+        let result = BlindedUTXO::new(body.blinded_utxo);
+        assert!(result.is_ok());
     }
 
     #[actix_web::test]
     async fn test_put_with_bad_request() {
         let app = test::init_service(App::new().service(put)).await;
-        let payload = Bech32Invoice {
-            bech32_invoice: ("helloRGB").to_string(),
+        let payload = RgbInvoice {
+            invoice_string: ("helloRGB").to_string(),
         };
         let req = test::TestRequest::put()
             .uri("/wallet/invoice")
